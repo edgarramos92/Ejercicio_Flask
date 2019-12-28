a3e877e4eb8e8001eacbd739c424bc226604daa7
Edgar Ramos <edgaromarall@hotmail.com>
2019-12-27 18:10:26 -0600

    Agregando carpeta con avances de aplicación de Flask

---
+++ Ejercicio_Flask/static/js/logging.js
@@ -0,0 +1,17 @@
+$(function(){
+	$('#btnLogging').click(function(){
+		
+		$.ajax({
+			url: '/Logging',
+			data: $('form').serialize(),
+			type: 'POST',
+			
+			success: function(response){
+				console.log(response);
+			},
+			error: function(error){
+				console.log(error);
+			}
+		});
+	});
+});
